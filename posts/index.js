a1f5f84682253880fa55d5f2184fe40f05bf857d
oss.harits <haritsmedina@gmail.com>
2021-12-19 21:42:47 +0700

    replace project

--- posts/index.js
+++ posts/index.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const axios = require('axios')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 
-const {randomBytes} = require ('crypto')
-const router = require('../posts/router/index')
+const app = express();
+app.use(bodyParser.json());
 
+const posts = {};
 
-const app = express()
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
 
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
-app.use(router)
+  posts[id] = {
+    id,
+    title
+  };
 
-app.listen(4000, ()=>{
-    console.log('listening 4000')
-})
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
