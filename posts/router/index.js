4bf82983e2a903b48ad57bb374d890d8c5924fde
oss.harits <haritsmedina@gmail.com>
2022-01-02 19:57:17 +0700

    postlist

--- posts/router/index.js
+++ posts/router/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const controller = require('../controller/index')
 
 router.get('/test',controller.getData)
-router.post('/test',controller.postData)
+router.post('/posts',controller.postData)
 router.get('/test/:id',controller.getDetail)
 
 module.exports = router
